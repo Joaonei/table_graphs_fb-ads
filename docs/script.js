297ad689d951c9e7b4a43457567742953ccc1c38
Joao Nei <joaonfagundes26@gmail.com>
2023-04-19 13:20:30 -0300

    first commit

---
+++ docs/script.js
@@ -0,0 +1,9 @@
+var click = document.querySelector(".click");
+
+
+click.addEventListener("click",()=> {
+
+    document.querySelector("#button").style.display = "none"
+    document.querySelector(".grafico").style.display = "flex"
+
+})
