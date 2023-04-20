8c9d10fbc79d6fadd4a16bc3a2f537d5eb9b1971
Joao Nei <joaonfagundes26@gmail.com>
2023-04-20 18:25:42 -0300

    add bottom

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,13 @@
 var click = document.querySelector(".click");
-
+var voltar = document.querySelector(".voltar");
 
 click.addEventListener("click",()=> {
 
     document.querySelector("#button").style.display = "none"
     document.querySelector(".grafico").style.display = "flex"
 
+})
+voltar.addEventListener("click", ()=> { 
+    document.querySelector(".grafico").style.display = "none"
+    document.querySelector("#button").style.display = "flex"
 })
